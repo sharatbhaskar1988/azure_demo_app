13dc5c2182b56a8c8c815fcb7423e62c369c20f8
sharatbhaskar1988 <69539456+sharatbhaskar1988@users.noreply.github.com>
2026-04-28 16:16:41 +0200

    Update db.py

--- db.py
+++ db.py
@@ -1,18 +1,14 @@
 import pyodbc
 
-# FOR LEARNING: tum in details ko environment variables me bhi daal sakte ho (baad me)
 SERVER = "sqlserver123.database.windows.net"
 DATABASE = "demodb"
-USERNAME = "azureadmin"
-PASSWORD = "TeraPassword123!"
 
 def get_db_connection():
     conn_str = (
         f"DRIVER={{ODBC Driver 18 for SQL Server}};"
         f"SERVER={SERVER};"
         f"DATABASE={DATABASE};"
-        f"UID={USERNAME};"
-        f"PWD={PASSWORD};"
+        f"Authentication=Active Directory Default;"
         f"Encrypt=yes;"
         f"TrustServerCertificate=no;"
         f"Connection Timeout=30;"
